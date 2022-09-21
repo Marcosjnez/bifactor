5f954bf8e0e55990405e93d6bf091611aafe7598
Marcos Jiménez <39010436+Marcosjnez@users.noreply.github.com>
2022-09-22 01:18:09 +0200

    Allow supplying custom lambda and Phi matrices

--- R/sim_factor.R
+++ R/sim_factor.R
@@ -537,6 +537,7 @@ yuan <- function(R, lambda, Phi, uniquenesses,
 #' @param fit Fit index to control the population error.
 #' @param misfit Misfit value to generate population error.
 #' @param error_method Method used to control population error: c("yuan", "cudeck"). Defaults to "yuan".
+#' @param lambda Custom matrix
 #'
 #' @details \code{sim_factor} generates bi-factor and generalized bifactor patterns with cross-loadings, pure items and
 #' correlations among the general and group factors. When \code{crossloading} is different than 0, one cross-loading
@@ -560,157 +561,191 @@ yuan <- function(R, lambda, Phi, uniquenesses,
 #' Jiménez, M., Abad, F.J., Garcia-Garzon, E., Garrido, L.E. (2021, June 24). Exploratory bi-factor analysis with multiple general factors. Under review. Retrieved from https://osf.io/7aszj/?view_only=8f7bd98025104347a96f60a6736f5a64
 #'
 #' @export
-sim_factor <- function(n_generals, groups_per_general, items_per_group,
+sim_factor <- function(n_generals = 0, groups_per_general = 5,
+                       items_per_group = 6,
                        loadings_g = "medium", loadings_s = "medium",
                        crossloadings = 0, pure = FALSE,
                        generals_rho = 0, groups_rho = 0,
                        confirmatory = TRUE, method = "minres",
-                       fit = "rmsr", misfit = 0, error_method = "yuan") {
+                       fit = "rmsr", misfit = 0, error_method = "yuan",
+                       lambda = NULL, Phi = NULL) {
 
-  ng <- n_generals
+  ng <- n_generals # Save the number of general factors for recursive iteration
   condition <- n_generals == 0
+  # Create a general factor by default, and then remove it upon the condition:
   if(condition) n_generals <- 1
 
-  if(crossloadings > 0.4) {
+  lambda_null <- is.null(lambda)
+  Phi_null <- is.null(Phi)
 
-    stop("Crossloadings are too large")
+  if(!lambda_null & Phi_null) {
 
-  }
+    # If lambda is provided but not Phi, then create Phi correlating all
+    # the factors at the value given by groups_rho
 
-  # Configure loadings:
-
-  if(is.numeric(loadings_g)) {
-    loadings_g. = loadings_g # custom loadings on the general factors
-  } else {
-    if(loadings_g == "low") {
-      loadings_g. = c(.3, .5)
-    } else if(loadings_g == "medium") {
-      loadings_g. = c(.4, .6)
-    } else if(loadings_g == "high") {
-      loadings_g. = c(.5, .7)
-    }
-  }
-
-  if(is.numeric(loadings_s)) {
-    loadings_s. = loadings_s # custom loadings on the group factors
-  } else {
-    if(loadings_s == "low") {
-      loadings_s. = c(.3, .5)
-    } else if(loadings_s == "medium") {
-      loadings_s. = c(.4, .6)
-    } else if(loadings_s == "high") {
-      loadings_s. = c(.5, .7)
-    }
-  }
-
-  # Total number of group factors:
-  n_groups <- n_generals * groups_per_general
-
-  # Total number of items:
-  n_items <- n_groups * items_per_group
-
-  # Number of items per general factor:
-  items_per_general <- n_items / n_generals
-
-  # Total number of factors:
-  n_factors <- n_generals + n_groups
-
-  # Initialize the population loading matrix:
-  lambda <- matrix(NA, nrow = n_items, ncol = n_factors)
-
-  # Item loadings on the group factors:
-
-  sequen <- seq(loadings_s.[2], loadings_s.[1], length.out = items_per_group)
-
-  for(i in 0:(n_groups-1)) {
-
-    start_row <- 1 + i*items_per_group
-    end_row <- start_row + items_per_group - 1
-    lambda[start_row:end_row , 1+i+n_generals] <- sequen
-    # lambda[start_row:end_row , 1+i+n_generals] <- mean(loadings_s.)
+    nfactors <- ncol(lambda)
+    Phi <- matrix(groups_rho, nfactors, nfactors)
+    diag(Phi) <- 1
 
   }
 
-  # Simulate item loadings on the general factors froma uniform distribution:
+  if(lambda_null) {
 
-  for(i in 0:(n_generals-1)) {
+    # Configure loadings:
 
-    start_row <- 1 + i*items_per_general
-    end_row <- start_row + items_per_general - 1
-    lambda[start_row:end_row , i+1] <- stats::runif(items_per_general, loadings_g.[1], loadings_g.[2])
-    # lambda[start_row:end_row , i+1] <- mean(loadings_g.)
+    if(crossloadings > 0.4) {
 
-  }
-
-  colnames(lambda) <- c(paste("G", 1:n_generals, sep = ""), paste("S", 1:n_groups, sep = ""))
-
-  # Pure items on the general factors:
-
-  if(pure) {
-
-    value <- sequen[floor(items_per_group/2 + 1)]
-    row_indexes <- unlist(apply(lambda, 2, FUN = function(x) which(x == value)))
-    column_indexes <- apply(lambda[row_indexes, ], 1, FUN = function(x) which(x > 0))
-    n <- n_groups * n_generals
-    indexes <- which(!is.na(lambda[row_indexes, 1:n_generals]))
-    m <- sqrt(lambda[row_indexes, 1:n_generals][indexes]^2 +
-                lambda[row_indexes, ][which(lambda[row_indexes, ] == value)]^2)
-    lambda[row_indexes, 1:n_generals][indexes] <- m
-    lambda[row_indexes, ][which(lambda[row_indexes, ] == value)] <- 0.01
-
-  }
-
-  # Cross-loadings on the group factors:
-
-  if(crossloadings != 0) {
-
-    ratio <- groups_per_general
-    row_index <- seq(items_per_group+1, n_items, by = items_per_group)
-    col_index <- seq(n_generals+1, n_factors-1, by = 1)
-
-    if(ratio < length((row_index))) {
-      delete <- seq(ratio, length(row_index), by = ratio)
-      row_index <- row_index[-delete]
-      col_index <- col_index[-delete]
-    }
-
-    # Insert cross-loadings and then recalibrate the loadings on the general and group factors to maintain the previous communality:
-
-    for(i in 1:length(row_index)) {
-
-      row_indexes <- row_index[i]:(row_index[i])
-      col_index_2 <- which(lambda[row_indexes[1], ] > 0)
-      lambda[row_indexes, col_index[i]] <- crossloadings
-      lambda[row_indexes, col_index_2] <- sqrt(lambda[row_indexes, col_index_2]^2 - crossloadings^2/2)
+      stop("Crossloadings are too large")
 
     }
 
-    for(i in 1:n_generals) {
+    if(is.numeric(loadings_g)) {
+      loadings_g. = loadings_g # custom loadings on the general factors
+    } else {
+      if(loadings_g == "low") {
+        loadings_g. = c(.3, .5)
+      } else if(loadings_g == "medium") {
+        loadings_g. = c(.4, .6)
+      } else if(loadings_g == "high") {
+        loadings_g. = c(.5, .7)
+      }
+    }
 
-      row_index <- items_per_general*(i-1)+1
-      row_indexes <- row_index:(row_index)
-      col_index_2 <- which(lambda[row_indexes[1], ] > 0)
-      lambda[row_indexes, n_generals+i*ratio] <- crossloadings
-      lambda[row_indexes, col_index_2] <- sqrt(lambda[row_indexes, col_index_2]^2 - crossloadings^2/2)
+    if(is.numeric(loadings_s)) {
+      loadings_s. = loadings_s # custom loadings on the group factors
+    } else {
+      if(loadings_s == "low") {
+        loadings_s. = c(.3, .5)
+      } else if(loadings_s == "medium") {
+        loadings_s. = c(.4, .6)
+      } else if(loadings_s == "high") {
+        loadings_s. = c(.5, .7)
+      }
+    }
+
+    # Total number of group factors:
+    n_groups <- n_generals * groups_per_general
+
+    # Total number of items:
+    n_items <- n_groups * items_per_group
+
+    # Number of items per general factor:
+    items_per_general <- n_items / n_generals
+
+    # Total number of factors:
+    n_factors <- n_generals + n_groups
+
+    if(!Phi_null) {
+
+      if(nrow(Phi) != n_factors & ncol(Phi) != n_factors) {
+        stop("The Phi matrix that was provided is not conformable with the \n
+             lambda matrix generated by the argument specifications")
+      }
 
     }
 
+    # Initialize the population loading matrix:
+    lambda <- matrix(NA, nrow = n_items, ncol = n_factors)
+
+    # Item loadings on the group factors:
+
+    sequen <- seq(loadings_s.[2], loadings_s.[1], length.out = items_per_group)
+
+    for(i in 0:(n_groups-1)) {
+
+      start_row <- 1 + i*items_per_group
+      end_row <- start_row + items_per_group - 1
+      lambda[start_row:end_row , 1+i+n_generals] <- sequen
+      # lambda[start_row:end_row , 1+i+n_generals] <- mean(loadings_s.)
+
+    }
+
+    # Simulate item loadings on the general factors froma uniform distribution:
+
+    for(i in 0:(n_generals-1)) {
+
+      start_row <- 1 + i*items_per_general
+      end_row <- start_row + items_per_general - 1
+      lambda[start_row:end_row , i+1] <- stats::runif(items_per_general, loadings_g.[1], loadings_g.[2])
+      # lambda[start_row:end_row , i+1] <- mean(loadings_g.)
+
+    }
+
+    colnames(lambda) <- c(paste("G", 1:n_generals, sep = ""), paste("S", 1:n_groups, sep = ""))
+
+    # Pure items on the general factors:
+
+    if(pure) {
+
+      value <- sequen[floor(items_per_group/2 + 1)]
+      row_indexes <- unlist(apply(lambda, 2, FUN = function(x) which(x == value)))
+      column_indexes <- apply(lambda[row_indexes, ], 1, FUN = function(x) which(x > 0))
+      n <- n_groups * n_generals
+      indexes <- which(!is.na(lambda[row_indexes, 1:n_generals]))
+      m <- sqrt(lambda[row_indexes, 1:n_generals][indexes]^2 +
+                  lambda[row_indexes, ][which(lambda[row_indexes, ] == value)]^2)
+      lambda[row_indexes, 1:n_generals][indexes] <- m
+      lambda[row_indexes, ][which(lambda[row_indexes, ] == value)] <- 0.01
+
+    }
+
+    # Cross-loadings on the group factors:
+
+    if(crossloadings != 0) {
+
+      ratio <- groups_per_general
+      row_index <- seq(items_per_group+1, n_items, by = items_per_group)
+      col_index <- seq(n_generals+1, n_factors-1, by = 1)
+
+      if(ratio < length((row_index))) {
+        delete <- seq(ratio, length(row_index), by = ratio)
+        row_index <- row_index[-delete]
+        col_index <- col_index[-delete]
+      }
+
+      # Insert cross-loadings and then recalibrate the loadings on the general and group factors to maintain the previous communality:
+
+      for(i in 1:length(row_index)) {
+
+        row_indexes <- row_index[i]:(row_index[i])
+        col_index_2 <- which(lambda[row_indexes[1], ] > 0)
+        lambda[row_indexes, col_index[i]] <- crossloadings
+        lambda[row_indexes, col_index_2] <- sqrt(lambda[row_indexes, col_index_2]^2 - crossloadings^2/2)
+
+      }
+
+      for(i in 1:n_generals) {
+
+        row_index <- items_per_general*(i-1)+1
+        row_indexes <- row_index:(row_index)
+        col_index_2 <- which(lambda[row_indexes[1], ] > 0)
+        lambda[row_indexes, n_generals+i*ratio] <- crossloadings
+        lambda[row_indexes, col_index_2] <- sqrt(lambda[row_indexes, col_index_2]^2 - crossloadings^2/2)
+
+      }
+
+    }
+
+    lambda[is.na(lambda)] <- 0
+    rownames(lambda) <- paste("item", 1:nrow(lambda), sep = "")
+
   }
 
-  lambda[is.na(lambda)] <- 0
-  rownames(lambda) <- paste("item", 1:nrow(lambda), sep = "")
+  if(Phi_null) {
 
-  # Factor correlations:
+    # Factor correlations:
 
-  Phi <- matrix(0, n_factors, n_factors)
-  Phi[1:n_generals, 1:n_generals] <- generals_rho
-  Phi[-(1:n_generals), -(1:n_generals)] <- groups_rho
-  diag(Phi) <- 1
+    Phi <- matrix(0, n_factors, n_factors)
+    Phi[1:n_generals, 1:n_generals] <- generals_rho
+    Phi[-(1:n_generals), -(1:n_generals)] <- groups_rho
+    diag(Phi) <- 1
 
-  if(condition) { # if n_generals == 0, remove the general factor
+    if(condition) { # if n_generals == 0, remove the general factor
 
-    lambda <- lambda[, -1, drop = FALSE]
-    Phi <- Phi[-1, , drop = FALSE][, -1, drop = FALSE]
+      lambda <- lambda[, -1, drop = FALSE]
+      Phi <- Phi[-1, , drop = FALSE][, -1, drop = FALSE]
+
+    }
 
   }
 
@@ -725,6 +760,10 @@ sim_factor <- function(n_generals, groups_per_general, items_per_group,
   # Execute sim_factor recursively until no communality is greater than 1:
 
   if( any(uniquenesses < 0) ) {
+
+    if(lambda_null | Phi_null) {
+      stop("The provided lambda or Phi matrix produced negative variances")
+    }
 
     warning("At least one communality greater than 1 was found \n Resampling...")
 
