04adc423f199cac6b4dd02c008ea23b9f7f24fd1
Víthor Rosa Franco <10902884+vthorrf@users.noreply.github.com>
2024-04-30 13:44:33 -0300

    Additional tetrad indices
    Different calculations of the tfi

--- R/tfi.R
+++ R/tfi.R
@@ -1,5 +1,5 @@
 #' @export
-tetrad_fit_index <- function(SampleCov, ImpliedCov) {
+tetrad_fit_index <- function(SampleCov, ImpliedCov, type="rmsd") {
   ## Get all the possible four-tuple of variables
   possibilities <- combn(1:ncol(SampleCov), 4)
 
@@ -15,8 +15,30 @@ tetrad_fit_index <- function(SampleCov, ImpliedCov) {
   }
 
   ## Calculate and return the tetrad fit index
-  #TFI <- 1 - mean(sqrt(c(res1[,5:7] - res0[,5:7])^2))
-  #TFI <- 1 - mean(abs(c(res1[,5:7] - res0[,5:7])/2))
-  TFI <- 1 - mean(abs(c(res1[,5:7] - res0[,5:7])))
+  er <- c(res1[,5:7] - res0[,5:7])
+  if(type == "mad") {
+    TFI <- 1 - mean(abs(er))
+  } else if(type == "rmsd") {
+    TFI <- 1 - sqrt(mean(er^2))
+  } else if(type == "fvu"){
+    TFI <- 1 - {var(er)/var(c(res0[,5:7]))}
+  } else if(type == "test"){
+    TFI <- 1 - mean(abs(er/2))
+  } else if(type == "wellfare"){
+    n <- length(er)
+    i <- rank(abs(er))
+    weight <- (2*n + 1 - 2*i)/(n^2)
+    TFI <- 1 - sum(weight * er)
+  } else if(type == "dich"){
+    diff0 <- sqrt(mean(er[c(res1[,5:7]) == 0]^2))
+    diff1 <- mean(abs(er[c(res1[,5:7]) != 0]))
+    if(length(diff0) == 0) {
+      TFI <- 1 - diff1
+    } else {
+      TFI <- 1 - ((diff0 + diff1)/2)
+    }
+  } else {
+    stop("Unkown tetrad type!")
+  }
   return(TFI)
 }
